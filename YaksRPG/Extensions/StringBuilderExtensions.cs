df3e5fc1859447c5d0bca45e75f9c1a82c20fd32
Zakary Boven <zakaryboven@gmail.com>
2023-08-28 13:46:41 +1200

    Append filler at start

--- YaksRPG/Extensions/StringBuilderExtensions.cs
+++ YaksRPG/Extensions/StringBuilderExtensions.cs
@@ -8,6 +8,8 @@ public static class StringBuilderExtensions
   /// <summary>Appends an <see cref="ICharacterClass"/> to the <see cref="StringBuilder"/>.</summary>
   public static void AppendCharacterClass(this StringBuilder stringBuilder, ICharacterClass characterClass)
   {
+    stringBuilder.AppendFiller();
+    
     stringBuilder.Append($$$"""
                             {{wide
                             ## {{{characterClass.Name}}}
@@ -15,9 +17,53 @@ public static class StringBuilderExtensions
 
                             {{{characterClass.Flavour}}}
                             """);
+    
+    stringBuilder.AppendMajorFeaturesHeader();
+    stringBuilder.AppendMinorFeaturesHeader();
 
     foreach (var feature in characterClass.Features)
       stringBuilder.AppendFeature(feature);
+  }
+
+  private static void AppendFiller(this StringBuilder stringBuilder)
+  {
+    stringBuilder.Append("""
+                         {{margin-top:700px
+
+                         }}
+
+                         # {{font-size:158px Yak's}}<br />Fantasy RPG
+
+                         \page
+                         {{pageNumber,auto}}
+                         {{footnote Table of Contents}}
+
+
+                         {{toc,wide,column-count:1
+                         # Table Of Contents
+                         
+                           - ## [{{ Introduction}}{{ 2}}](#p3)
+                           - ## [{{ Berserker}}{{ 3}}](#p4)
+                         }}
+
+                         \page
+                         {{pageNumber,auto}}
+                         {{footnote Summary}}
+
+                         {{wide
+                         ## Introduction
+                         }}
+                         Introduction goes here.
+
+                         ### Foreword
+                           Foreword goes here.
+                         {{wide,padding-top:350px,padding-bottom:100px
+
+                         #### Credits
+                         
+                             Credit goes here.
+                         }}
+                         """);
   }
 
   /// <summary>Appends a <see cref="Feature"/> to the <see cref="StringBuilder"/>.</summary>
@@ -28,6 +74,22 @@ public static class StringBuilderExtensions
                          {{feature
                          **{{{feature.Name}}}:** {{{feature.Description}}}
                          }}
+                         """);
+  }
+
+  private static void AppendMajorFeaturesHeader(this StringBuilder stringBuilder)
+  {
+    stringBuilder.Append("""
+                         
+                         #### Major Features
+                         """);
+  }
+  
+  private static void AppendMinorFeaturesHeader(this StringBuilder stringBuilder)
+  {
+    stringBuilder.Append("""
+                         
+                         #### Minor Features
                          """);
   }
 }
