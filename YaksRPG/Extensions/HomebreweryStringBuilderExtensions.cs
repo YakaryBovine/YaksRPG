fd8560955e1635062808b80d2b3b7416c7e4a01d
Zakary Boven <zakaryboven@gmail.com>
2023-08-29 18:08:38 +1200

    Append class progression table

--- YaksRPG/Extensions/HomebreweryStringBuilderExtensions.cs
+++ YaksRPG/Extensions/HomebreweryStringBuilderExtensions.cs
@@ -5,35 +5,11 @@ namespace YaksRPG.Extensions;
 
 public static class HomebreweryStringBuilderExtensions
 {
-  /// <summary>Appends an <see cref="ICharacterClass"/> to the <see cref="StringBuilder"/>.</summary>
-  public static void AppendCharacterClass(this StringBuilder stringBuilder, ICharacterClass characterClass)
-  {
-    stringBuilder.AppendClassPageHeader(characterClass);
-    stringBuilder.AppendClassHeading(characterClass);
-    stringBuilder.AppendFeatures(characterClass, FeatureType.Core);
-    stringBuilder.AppendFeatures(characterClass, FeatureType.Major);
-    stringBuilder.AppendFeatures(characterClass, FeatureType.Minor);
-  }
-
-  private static void AppendFeatures(this StringBuilder stringBuilder, ICharacterClass characterClass, FeatureType featureType)
-  {
-    var matchingFeatures = characterClass.Features.Where(x => x.Type == featureType).ToList();
-    if (!matchingFeatures.Any())
-      return;
-    
-    stringBuilder.Append($"""
-
-                          #### {featureType.GetName()} Features
-                          """);
-    foreach (var feature in matchingFeatures)
-      stringBuilder.AppendFeature(feature);
-  }
-
   public static void AppendFiller(this StringBuilder stringBuilder)
   {
-    stringBuilder.Append("""
+    stringBuilder.AppendLine("""
                          ![bg main](https://images.squarespace-cdn.com/content/v1/548a736be4b04f6e8e823d49/1596045860203-OCM1RXRUY9KWLF2AB4Q2/Avartar-4-naomi-vandoren-web1080-crop.jpg?format=1000w) {position:absolute;top:-150px;left:-180px;height:1210px;}
-                         
+
                          {{margin-top:700px
 
                          }}
@@ -66,16 +42,52 @@ public static class HomebreweryStringBuilderExtensions
                          {{wide,padding-top:350px,padding-bottom:100px
 
                          #### Credits
-                         
+
                          Credit goes here.
                          }}
                          """);
   }
+  
+  /// <summary>Appends an <see cref="ICharacterClass"/> to the <see cref="StringBuilder"/>.</summary>
+  public static void AppendCharacterClass(this StringBuilder stringBuilder, ICharacterClass characterClass)
+  {
+    stringBuilder.AppendClassPageHeader(characterClass);
+    stringBuilder.AppendClassHeading(characterClass);
+    stringBuilder.AppendClassProgressionTable(characterClass);
+    stringBuilder.AppendFeatures(characterClass, FeatureType.Core);
+    stringBuilder.AppendFeatures(characterClass, FeatureType.Major);
+    stringBuilder.AppendFeatures(characterClass, FeatureType.Minor);
+  }
+
+  private static void AppendClassProgressionTable(this StringBuilder stringBuilder, ICharacterClass characterClass)
+  {
+    stringBuilder.AppendLine("""
+                         | Level | Hit Dice | Attack Bonus | Major Techniques | Minor Techniques |
+                         |:-:|:-:|:-:|:--|:--|
+                         """);
+    for (var level = 0; level < 10; level++)
+      stringBuilder.AppendClassProgressionTableLine(characterClass, level);
+  }
+
+  private static void AppendClassProgressionTableLine(this StringBuilder stringBuilder, ICharacterClass characterClass, int level)
+  {
+    stringBuilder.AppendLine($"| {level} | {characterClass.AttackBonusPerLevel} | {characterClass.AttackBonusPerLevel*level} | {level} | { level/4 }");
+  }
+  
+  private static void AppendFeatures(this StringBuilder stringBuilder, ICharacterClass characterClass, FeatureType featureType)
+  {
+    var matchingFeatures = characterClass.Features.Where(x => x.Type == featureType).ToList();
+    if (!matchingFeatures.Any())
+      return;
+    
+    stringBuilder.AppendLine($"#### {featureType.GetName()} Features");
+    foreach (var feature in matchingFeatures)
+      stringBuilder.AppendFeature(feature);
+  }
 
   private static void AppendClassHeading(this StringBuilder stringBuilder, ICharacterClass characterClass)
   {
-    stringBuilder.Append($$$"""
-
+    stringBuilder.AppendLine($$$"""
                             {{wide
                             ## {{{characterClass.Name}}}
                             }}
@@ -86,8 +98,7 @@ public static class HomebreweryStringBuilderExtensions
   
   private static void AppendClassPageHeader(this StringBuilder stringBuilder, ICharacterClass characterClass)
   {
-    stringBuilder.Append($$$"""
-                         
+    stringBuilder.AppendLine($$$"""
                          \page
                          {{pageNumber,auto}}
                          {{footnote {{{characterClass.Name}}}}}
@@ -96,8 +107,7 @@ public static class HomebreweryStringBuilderExtensions
   
   private static void AppendFeature(this StringBuilder stringBuilder, Feature feature)
   {
-    stringBuilder.Append($$$"""
-                         
+    stringBuilder.AppendLine($$$"""
                          {{feature
                          **{{{feature.Name}}}:** {{{feature.Description}}}
                          }}
